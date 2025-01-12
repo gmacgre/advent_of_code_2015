2f3dcc11426bd418f9cec097b33f53b5856450c8
gmacgre <gmacgre@byu.edu>
2025-01-12 09:37:21 -0600

    Add 24-1 Solution Add 24-2 Solution

---
+++ 24/24-2.py
@@ -0,0 +1,13 @@
+import sys, itertools, math
+from functools import reduce
+input = list(map(int, sys.stdin.read().split('\n')))
+target = sum(input) // 4
+quantum_entanglement = None
+for i in range(1, len(input)):
+    for combo in itertools.combinations(input, i):
+        if sum(combo) == target:
+            quantum_entanglement = reduce(lambda x, y: x * y, combo)
+            break
+    if quantum_entanglement != None:
+        break
+print(quantum_entanglement)
